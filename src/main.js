4845a88aa9395f682ebf58c95c8c51877159b195
namanprat <59867991+namanprat@users.noreply.github.com>
2025-07-06 12:00:07 +0530

    -

--- src/main.js
+++ src/main.js
@@ -4,23 +4,15 @@ import 'lenis/dist/lenis.css'
 import three from './work.js'
 
 
-const lenis = new Lenis({
-  autoRaf: true,
-});
+ const lenis = new Lenis();
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
-// const lenis = new Lenis();
+ lenis.on('scroll', ScrollTrigger.update);
+ gsap.ticker.add((time) => {
+   lenis.raf(time * 1000);
+ });
+ gsap.ticker.lagSmoothing(0);
 
-// lenis.on('scroll', ScrollTrigger.update);
-// gsap.ticker.add((time) => {
-//   lenis.raf(time * 1000);
-// });
-// gsap.ticker.lagSmoothing(0);
-
-
+three()
 
 console.log("%cDesigned and built by https://namanprat.com", "background:blue;color:#fff;padding: 8px;");
 {/* <script type="module" src="https://perception-pod.netlify.app/main.js"></script> */}
